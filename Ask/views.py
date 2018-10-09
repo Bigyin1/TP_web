79fd8479081dc63b6bfe3cae343b8cfbd5493f25
Bigyin <sergei.aparin1955@yandex.ru>
2018-10-09 16:14:35 +0300

    question_details_added

--- Ask/views.py
+++ Ask/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def test(request):
-    return render(request, "add_question.html", {})
+    return render(request, "question_details.html", {})
 
 # Create your views here.
