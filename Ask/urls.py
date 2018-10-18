0e8a8d84d5261e6066f34ea394ae3f081e072544
Bigyin <sergei.aparin1955@yandex.ru>
2018-10-19 01:50:20 +0300

    DZ 1 and DZ 2 done

--- Ask/urls.py
+++ Ask/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from Ask.views import test
+from Ask.views import *
 
 urlpatterns = [
-    path('', test),
+    path('', feed, name='main'),
+    path('hot', feed, name='hot'),
+    path('ask', ask, name='ask'),
+    path('question/<int:id>', question, name='question'),
+    path('login', login, name="login"),
+    path('signup', signup, name="signup"),
+    path('profile', profile, name='profile'),
+    path('tag/<str:tag_name>', tag, name='tag'),
 ]
--- Ask/views.py
+++ Ask/views.py
@@ -1,7 +1,71 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
-def test(request):
-    return render(request, "question.html", {})
+def paginator(request, query):
+    paginator = Paginator(query, 3)
+    page = request.GET.get('page')
+    question_list = paginator.get_page(page)
+    return question_list
 
+
+def feed(request):
+    questions = []
+    for i in range(1, 50):
+        questions.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'text': 'text' + str(i)
+        })
+    return render(request, "feed.html", {'questions': paginator(request, questions)})
+
+
+def hot(request):
+    questions = []
+    for i in range(1, 20):
+        questions.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'text': 'text' + str(i)
+        })
+    return render(request, "feed.html", {'questions': paginator(request, questions)})
+
+
+def ask(request):
+    return render(request, "ask.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def question(request, id):
+    comments = []
+    for i in range(1, 5):
+        comments.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'text': 'text' + str(i)
+        })
+    return render(request, "question.html", {'comments': comments})
+
+
+def signup(request):
+    return render(request, "signup.html", {})
+
+
+def profile(request):
+    return render(request, "user_profile.html", {})
+
+
+def tag(request, tag_name):
+    questions = []
+    for i in range(1, 30):
+        questions.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'text': 'text' + str(i)
+        })
+    return render(request, "tag_questions.html", {'questions': paginator(request, questions)})
 # Create your views here.
